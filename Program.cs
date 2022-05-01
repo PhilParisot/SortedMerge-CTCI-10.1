3223aae2709a0d2e04360717b5573c5527af74b5
Philippe Parisot <pparisot3@gmail.com>
2022-04-30 22:29:11 -0400

    fixed bug when A would access out of bounds

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿
 
 
-var result = SortedMerge(new List<int> { 1, 4, 6, 7, 9 }, new int[] { 2, 3, 5, 6, 9 });
+var result = SortedMerge(new List<int> { 16, 17  }, new int[] { 2, 3, 5, 6, 9, 9, 10 });
 System.Console.WriteLine();                         // 1 2 3 4 5 6 6 7 9 9
 
 static int[] SortedMerge(List<int> A, int[] B)
@@ -9,12 +9,18 @@ static int[] SortedMerge(List<int> A, int[] B)
     int IndexA = 0;
     foreach (int i in B)
     {
-        while (A[IndexA] < i)
+        if (IndexA > A.Count - 1)
+            A.Add(i);
+        else
         {
+            while (A[IndexA] < i)
+            {
+                IndexA++;
+                if (IndexA > A.Count - 1) break;
+            }
+            A.Insert(IndexA, i);
             IndexA++;
         }
-        A.Insert(IndexA, i);
-        IndexA++;
     }
     return A.ToArray();
 }
