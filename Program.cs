5c9fd74091b596d5b7922898c77938fa4e77c34a
Philippe Parisot <pparisot3@gmail.com>
2022-04-30 21:30:07 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+var result = SortedMerge(new List<int> { 1, 4, 6, 7, 9 }, new int[] { 2, 3, 5, 6, 9 });
+System.Console.WriteLine();
+
+static int[] SortedMerge(List<int> A, int[] B)
+{
+    int IndexA = 0;
+    foreach (int i in B)
+    {
+        while (A[IndexA] < i)
+        {
+            IndexA++;
+        }
+        A.Insert(IndexA, i);
+    }
+    return A.ToArray();
+}
